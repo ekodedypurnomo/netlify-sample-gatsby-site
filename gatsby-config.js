07fd584ff2e09dfd3492c1b53bef7af92ae496c7
Eko Dedy Purnomo <eko.sekawanmedia@gmail.com>
2020-04-23 13:22:55 +0700

    add markdown plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,13 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
+	{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'markdown-pages',
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
